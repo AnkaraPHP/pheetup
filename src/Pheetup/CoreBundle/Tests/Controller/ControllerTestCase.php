1e35d2d881fe4d30b4bdce2fc4c7d2eea812bfc0
Emre YILMAZ <emreyilmaz@yandex.com>
2016-03-15 00:29:16 +0200

    created controller test case.

---
+++ src/Pheetup/CoreBundle/Tests/Controller/ControllerTestCase.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * pheetup
+ * User: emreyilmaz
+ */
+
+namespace Pheetup\CoreBundle\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ControllerTestCase extends WebTestCase
+{
+    /** @var  string */
+    private $enviroment = 'test';
+
+    /** @var \Symfony\Component\DependencyInjection\ContainerInterface */
+    protected $container;
+
+    protected $controller;
+
+    public function __construct( $name = NULL, array $data = [ ], $dataName = "" )
+    {
+        parent::__construct( $name, $data, $dataName );
+        if ( !static::$kernel )
+        {
+            static::$kernel = self::createKernel( [
+                                                      'environment' => $this->enviroment,
+                                                      'debug'       => TRUE,
+                                                  ] );
+            static::$kernel->boot();
+        }
+
+        $this->container = self::$kernel->getContainer();
+    }
+
+    public function setController( $name )
+    {
+        $this->controller = $this->container->get( 'pheetup.controller.' . $name );
+
+        return $this;
+    }
+}
