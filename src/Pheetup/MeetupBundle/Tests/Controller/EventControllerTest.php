e9a2489b0dd436b8ce149d39e2ed0fc4b9c958cb
Emre YILMAZ <emreyilmaz@yandex.com>
2016-03-15 21:43:28 +0200

    added assertNotNull to create event test.

--- src/Pheetup/MeetupBundle/Tests/Controller/EventControllerTest.php
+++ src/Pheetup/MeetupBundle/Tests/Controller/EventControllerTest.php
@@ -21,26 +21,53 @@ class EventControllerTest extends ControllerTestCase
 
     public function testCreateEvent()
     {
-        /*
-         * 1- Request
-         * 2- title, description,start, finish,location,submit -> event[]
-         * 3- Request->Post
-         */
+        $key = 'Ankara, Turkiye ' . uniqid();
+
         $request = new Request();
         $request->setMethod( 'POST' );
-        $request->query->add( [
-                                  'event' => [
-                                      'title'       => 'AnkaraPHP Uzak Diyarlar',
-                                      'description' => 'Deneme',
-                                      'start'       => ( new \DateTime( "-3 days" ) )->format( "d-m-Y H:i:s" ),
-                                      'finish'      => ( new \DateTime( "-2 days" ) )->format( "d-m-Y H:i:s" ),
-                                      'submit'      => 'submit',
-                                      'location'    => 'Ankara, Turkiye',
-                                  ],
-                              ] );
+        $startDate  = new \DateTime( "-3 days" );
+        $finishDate = new \DateTime( "-2 days" );
+        $csrf       = $this->container->get( 'security.csrf.token_manager' );
+        $token      = $csrf->getToken( 'pheetup_meetupbundle_event' );
+        $request->request->set( 'pheetup_meetupbundle_event', [
+            'title'       => 'AnkaraPHP Uzak Diyarlar',
+            'description' => 'Deneme',
+            'start'       => [
+                'date' => [
+                    'year'  => (int)$startDate->format( 'Y' ),
+                    'month' => (int)$startDate->format( 'm' ),
+                    'day'   => (int)$startDate->format( 'd' ),
+                ],
+                'time' => [
+                    'hour'   => (int)$startDate->format( 'H' ),
+                    'minute' => (int)$startDate->format( 'i' ),
+                ],
+            ],
+            'finish'      => [
+                'date' => [
+                    'year'  => (int)$finishDate->format( 'Y' ),
+                    'month' => (int)$finishDate->format( 'm' ),
+                    'day'   => (int)$finishDate->format( 'd' ),
+                ],
+                'time' => [
+                    'hour'   => (int)$finishDate->format( 'H' ),
+                    'minute' => (int)$finishDate->format( 'i' ),
+                ],
+            ],
+            'submit'      => 'submit',
+            'location'    => $key,
+            '_token'      => $token,
+        ] );
         /** @var Response $response */
         $response = $this->controller->createAction( $request );
 
-        $this->assertEquals( 200, $response->getStatusCode() );
+        $this->assertEquals( 302, $response->getStatusCode() );
+
+        $em              = $this->container->get( 'doctrine.orm.entity_manager' );
+        $eventRepository = $em->getRepository( 'PheetupMeetupBundle:Event' );
+
+        $event = $eventRepository->findOneBy( [ 'location' => $key ] );
+
+        $this->assertNotNull( $event->getId() );
     }
 }
