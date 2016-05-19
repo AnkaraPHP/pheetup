23e82bc23139398f41defb6b366cabed88af1236
Ahmet Erşin <ahmetersin06@gmail.com>
2016-05-19 10:08:50 +0300

    GroupTests and UserBundle Tests fixed

--- src/Pheetup/SiteBundle/Tests/DefaultControllerTest.php
+++ src/Pheetup/SiteBundle/Tests/DefaultControllerTest.php
@@ -10,6 +10,7 @@ namespace Pheetup\SiteBundle\Tests\Controller;
 
 use Doctrine\ORM\EntityManager;
 use Pheetup\SiteBundle\Controller\DefaultController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Pheetup\CoreBundle\Tests\Controller\ControllerTestCase;
 
@@ -24,61 +25,61 @@ class DefaultControllerTest extends ControllerTestCase
         $this->em = $this->container->get('doctrine.orm.entity_manager');
     }
 
-    /**
-     * @todo DefaultControllerTest
-     * Grup testini yazamadığım için burayıda sonra halledeceğim
-     *
-    * private function createGroup($controller, $name, $domain)
-    * {
-        * $request = new Request();
-        * $request->setMethod('POST');
-        * $csrf = $this->container->get('security.csrf.token_manager');
-        * $token = $csrf->getToken('pheetup_userbundle_group');
-        * $request->request->set(
-            * 'pheetup_userbundle_group',
-            * [
-                * 'name' => $name,
-                * 'description' => "Ankara'da güzel bir grup",
-                * 'logo' => "img.png",
-                * 'location' => 'Ankara / Turkey',
-                * 'domain' => $domain,
-                * 'submit' => 'submit',
-                * '_token' => $token,
-            * ]
-        * );
- *
-* return $controller->createAction($request);
-    * }
- *
-     * */
+    private function createGroup($controller, $name, $domain)
+    {
+        $request = new Request();
+        $request->setMethod('POST');
+        $csrf = $this->container->get('security.csrf.token_manager');
+        $token = $csrf->getToken('pheetup_userbundle_group');
+        $dir = dirname($this->container->getParameter('kernel.root_dir')).'/src/Pheetup/UserBundle/Tests/Media/';
+        copy($dir.'test.jpg', $dir.'test1.jpg');
+        $path = $dir.'test1.jpg';
+        $logo = new UploadedFile($path, 'test.png', 'image/png', filesize($path), UPLOAD_ERR_OK, true);
+        $request->request->set(
+            'pheetup_userbundle_group',
+            [
+                'name' => $name,
+                'description' => "Ankara'da güzel bir grup",
+                'logo' => $logo,
+                'location' => 'Ankara / Turkey',
+                'domain' => $domain,
+                'submit' => 'submit',
+                '_token' => $token,
+            ]
+        );
+        $request->files->get('logo');
+
+        return $controller->createAction($request);
+    }
+
     public function testJoinAction()
-    {/*
-        * $name = "GrupDeneme";
-        * $domain = "denemegrup";
-        * $groupResponse = $this->createGroup($this->container->get('pheetup.controller.user.group'), $name, $domain);
-        * $request = new Request();
-        * $request->setMethod('GET');
-        * $groupRepository = $this->em->getRepository('PheetupUserBundle:Group');
-        * $group = $groupRepository->findOneBy(['name' => $name]);
-        * $this->assertNotEmpty($group->getId());;
-        * $userManager = $this->container->get('fos_user.user_manager');
-        * $user = $userManager->createUser();
-        * $user->setUsername('ahmet');
-        * $user->setEmail('ahmetersin06@gmail.com');
-        * $user->setPlainPassword('deneme');
-        * $user->setEnabled(true);
-        * $userManager->updateUser($user);
-        * $user = $userManager->findUserBy(array('username' => 'ahmet'));
-        * $loginManager = $this->container->get('fos_user.security.login_manager');
-        * $firewallName = $this->container->getParameter('fos_user.firewall_name');
-        * $loginManager->logInUser($firewallName, $user);
-        * $this->assertTrue($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY'));
-        * $controller = new DefaultController();
-        * $controller->setContainer($this->container);
-        * $response = $controller->joinAction($group);
-        * $this->assertEquals($response->getContent(), "You successfully joined group");
-     *
-     */
+    {
+        $name = "GrupDeneme";
+        $domain = "denemegrup";
+        $groupResponse = $this->createGroup($this->container->get('pheetup.controller.user.group'), $name, $domain);
+        $request = new Request();
+        $request->setMethod('GET');
+        $groupRepository = $this->em->getRepository('PheetupUserBundle:Group');
+        $group = $groupRepository->findOneBy(['name' => $name]);
+        $this->assertNotEmpty($group->getId());;
+        $userManager = $this->container->get('fos_user.user_manager');
+        $user = $userManager->createUser();
+        $user->setUsername('ahmet');
+        $user->setEmail('ahmetersin06@gmail.com');
+        $user->setPlainPassword('deneme');
+        $user->setEnabled(true);
+        $userManager->updateUser($user);
+        $user = $userManager->findUserBy(array('username' => 'ahmet'));
+        $loginManager = $this->container->get('fos_user.security.login_manager');
+        $firewallName = $this->container->getParameter('fos_user.firewall_name');
+        $loginManager->logInUser($firewallName, $user);
+        $this->assertTrue($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY'));
+        $controller = new DefaultController();
+        $controller->setContainer($this->container);
+        $response = $controller->joinAction($group);
+        $this->assertEquals($response->getContent(), "You successfully joined group");
+
+
     }
 
 }
--- src/Pheetup/UserBundle/Tests/Controller/GroupControllerTest.php
+++ src/Pheetup/UserBundle/Tests/Controller/GroupControllerTest.php
@@ -9,6 +9,7 @@
 namespace FOS\UserBundle\Tests\Controller;
 
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Pheetup\CoreBundle\Tests\Controller\ControllerTestCase;
 
@@ -22,56 +23,43 @@ class GroupControllerTest extends ControllerTestCase
         $this->setController('user.group');
         $this->em = $this->container->get('doctrine.orm.entity_manager');
     }
-    /**
-     * @todo GroupController Test
-     * Çok uğraştım ama form'u işlerken dosyayı görmedi bir türlü
-     * Bu testi daha sonra yazarım diye düşündüm
-     */
-    /**
-     * private function createGroup($name, $domain)
-     * {
-     * $this->assertTrue(file_exists(dirname($this->container->getParameter('kernel.root_dir')).'/src/Pheetup/UserBundle/Tests/Media/test.jpg'));
-     * $client = static::createClient();
-     * $csrf = $client->getContainer()->get('security.csrf.token_manager');
-     * $token = $csrf->getToken('pheetup_userbundle_group');
-     * $file = array(
-     * 'tmp_name' => dirname($this->container->getParameter('kernel.root_dir')).'/src/Pheetup/UserBundle/Tests/Media/test.jpg',
-     * 'name' => 'photo.jpg',
-     * 'type' => 'image/jpeg',
-     * 'size' => 115671,
-     * 'error' => UPLOAD_ERR_OK);
-     * $client->followRedirects(true);
-     * $crawler = $client->request(
-     * 'POST',
-     * '/group/create',
-     * ['pheetup_userbundle_group' =>
-     * [
-     * 'name' => $name,
-     * 'description' => "Ankara'da güzel bir grup",
-     * 'location' => 'Ankara / Turkey',
-     * 'domain' => $domain,
-     * 'submit' => '',
-     * '_token' => $token,
-     * ]
-     * ],
-     * [
-     * 'pheetup_userbundle_group[logo]'=>$file
-     * ]
-     * );
-     * file_put_contents('a.html',$crawler->html());
-     * }
-     **/
+
+    private function createGroup($controller, $name, $domain)
+    {
+        $request = new Request();
+        $request->setMethod('POST');
+        $csrf = $this->container->get('security.csrf.token_manager');
+        $token = $csrf->getToken('pheetup_userbundle_group');
+        $dir = dirname($this->container->getParameter('kernel.root_dir')).'/src/Pheetup/UserBundle/Tests/Media/';
+        copy($dir.'test.jpg', $dir.'test1.jpg');
+        $path = $dir.'test1.jpg';
+        $logo = new UploadedFile($path, 'test.png', 'image/png', filesize($path), UPLOAD_ERR_OK, true);
+        $request->request->set(
+            'pheetup_userbundle_group',
+            [
+                'name' => $name,
+                'description' => "Ankara'da güzel bir grup",
+                'logo' => $logo,
+                'location' => 'Ankara / Turkey',
+                'domain' => $domain,
+                'submit' => 'submit',
+                '_token' => $token,
+            ]
+        );
+        $request->files->get('logo');
+
+        return $controller->createAction($request);
+    }
 
     public function testCreateAction()
     {
-        /*
         $name = 'AnkaraPhp';
         $domain = 'ankaraphp';
-        $this->createGroup($name, $domain);
+        $response = $this->createGroup($this->controller, $name, $domain);
+        $this->assertEquals(302, $response->getStatusCode());
+
         $groupRepository = $this->em->getRepository('PheetupUserBundle:Group');
-        var_dump($groupRepository->findAll());
         $group = $groupRepository->findOneBy(['name' => $name]);
-        $this->assertNotEmpty($group->getDescription());
-         **/
+        $this->assertNotEmpty($group->getId());
     }
 }
